00530051052f32d15bdfc75f107cdb70abbc93ed
Youssef <choukriyoussef7905@gmail.com>
2022-07-06 17:49:28 +0100

    Create build_tree method

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require "pry-byebug"
+
 class Node
     include Comparable
     attr_accessor :data, :right_child, :left_child
@@ -14,7 +16,21 @@ end
 
 
 class BinaryTree
+    attr_reader :root
     def initialize(array)
-        @raw_data = array
+        @root = build_tree(array.sort.uniq)
+    end
+
+    def build_tree(array)
+        left_children = array.slice!(0, array.length/2)
+        right_children = array
+        middle = right_children.length == left_children.length ? left_children.pop : right_children.shift
+        
+
+        parent = Node.new(middle)
+        parent.left_child = build_tree(left_children) if !left_children.empty?
+        parent.right_child = build_tree(right_children) if !right_children.empty?
+        parent
     end
 end
+
